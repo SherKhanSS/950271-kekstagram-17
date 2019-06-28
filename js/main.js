267aa97ce363b79e2d927f3dd6ea92a3efb975a9
Юрий <gurovg@gmail.com>
2019-06-28 23:40:39 +0300

    добавляет масштабирование картинки

--- js/main.js
+++ js/main.js
@@ -88,13 +88,63 @@ buttonCancel.addEventListener('click', function() {
 var buttonSmaller = imgUpload.querySelector('.scale__control--smaller');
 var buttonBigger = imgUpload.querySelector('.scale__control--bigger');
 var inputScale = imgUpload.querySelector('.scale__control--value');
+var imgPreview = imgUpload.querySelector('.img-upload__preview');
+
+
 
 buttonSmaller.addEventListener('click', function() {
-  // inputScale.value = ?;
+  var scale = inputScale.value;
+    if(scale == "100%") {
+      return inputScale.value = "75%";
+    }
+    if(scale == "75%") {
+      return inputScale.value = "50%";
+    }
+    if(scale == "50%") {
+      return inputScale.value = "25%";
+    }
+});
+
+buttonSmaller.addEventListener('click', function() {
+  var scale = inputScale.value;
+    if(scale == "75%") {
+      return imgPreview.style.transform = "scale(0.75)";
+    }
+    if(scale == "50%") {
+      return imgPreview.style.transform = "scale(0.50)";
+    }
+    if(scale == "25%") {
+      return imgPreview.style.transform = "scale(0.25)";
+    }
+});
+
+buttonBigger.addEventListener('click', function() {
+  var scale = inputScale.value;
+    if(scale == "25%") {
+      return inputScale.value = "50%";
+    }
+    if(scale == "50%") {
+      return inputScale.value = "75%";
+    }
+    if(scale == "75%") {
+      return inputScale.value = "100%";
+    }
+});
+
+buttonBigger.addEventListener('click', function() {
+  var scale = inputScale.value;
+    if(scale == "50%") {
+      return imgPreview.style.transform = "scale(0.50)";
+    }
+    if(scale == "75%") {
+      return imgPreview.style.transform = "scale(0.75)";
+    }
+    if(scale == "100%") {
+      return imgPreview.style.transform = "scale(1)";
+    }
 });
 
 
-// var imgPreview = imgUpload.querySelector('.img-upload__preview');
 // var imgNone = imgUpload.querySelector('.effects__preview--none');
 // var imgChrome = imgUpload.querySelector('.effects__preview--chrome');
 // var imgSepia = imgUpload.querySelector('.effects__preview--sepia');
